1d13b85ff9292ba89f78e5701364b35e333ffb18
Saeid Mottaghi Tabarestani <saeidmot@gmail.com>
2023-07-01 19:56:02 +0330

    Correction

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'templates.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'media')
+    os.path.join(BASE_DIR,'static')
 
 ]
 MEDIA_URL='/media/'
